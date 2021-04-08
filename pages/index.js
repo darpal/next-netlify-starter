98003e3638473ce98efce6ec1903856c4e527621
Patrik Oskarsson <patrik.oskarsson@darpal.com>
2021-04-08 14:13:11 +0100

    test commit

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
-import Head from 'next/head'
-import Header from '@components/Header'
-import Footer from '@components/Footer'
+import Head from "next/head";
+import Header from "@components/Header";
+import Footer from "@components/Footer";
 
 export default function Home() {
   return (
@@ -13,11 +13,12 @@ export default function Home() {
       <main>
         <Header title="Welcome to my app!" />
         <p className="description">
+          <h1>This is Patriks header</h1>
           Get started by editing <code>pages/index.js</code>
         </p>
       </main>
 
       <Footer />
     </div>
-  )
+  );
 }
